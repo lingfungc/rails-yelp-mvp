89fb3a3505ffaf68c687f6ed8682a9ee7a396851
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-16 00:17:43 +0000

    completed restaurant-review relationship and validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  # validates :name, presence: true
+  # validates :address, presence: true
+  # validates :category, presence: true
+
+  validates :name, :address, :category, presence: true
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: { in: CATEGORIES }
+end
---
+++ procedures.rb
@@ -0,0 +1,8 @@
+1   set up and install gemfiles
+
+2   rails g model Restaurant ... / Review ... restaurant:references
+
+3   rails db:migrate RAILS_ENV=test
+    rspec
+
+4   update model 1:N relationship / validations
