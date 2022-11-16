299bea0fcb2694e05d344e265074223d28f1d41d
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-16 16:40:46 +0000

    added pg_search scope in Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,14 @@ class Restaurant < ApplicationRecord
   # validates :address, presence: true
   # validates :category, presence: true
 
-  validates :name, :address, :category, presence: true
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
