7571518f425c06ad5c1122fdc1d7ee200a7b3367
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-16 08:03:02 +0000

    added restaurants routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :show, :new, :create]
 end
